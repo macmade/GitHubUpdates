5fbe6469b6357bb6c4197b5954c6f74eb236d6e2
macmade <macmade@xs-labs.com>
2017-06-16 09:08:00 +0200

    Added the test application project.

---
+++ TestApp/Classes/MainWindowController.m
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2017 Jean-David Gadina - www.xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+/*!
+ * @file        MainWindowController.m
+ * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com
+ */
+
+#import "MainWindowController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainWindowController()
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+@implementation MainWindowController
+
+- ( instancetype )init
+{
+    return [ self initWithWindowNibName: NSStringFromClass( [ self class ] ) ];
+}
+
+- ( void )windowDidLoad
+{
+    [ super windowDidLoad ];
+    
+    self.window.titlebarAppearsTransparent = YES;
+}
+
+@end
