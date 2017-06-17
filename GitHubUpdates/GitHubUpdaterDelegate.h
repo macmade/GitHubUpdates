5735367b7c1bb85b71559d3e9de14629061f0c7b
macmade <macmade@xs-labs.com>
2017-06-17 17:51:13 +0200

    Inline documentation...

--- GitHubUpdates/GitHubUpdaterDelegate.h
+++ GitHubUpdates/GitHubUpdaterDelegate.h
@@ -35,23 +35,143 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/*!
+ * @protocol    GitHubUpdaterDelegate
+ * @abstract    Optional delegate for GitHubUpdater.
+ * @see         GitHubUpdater
+ */
 @protocol GitHubUpdaterDelegate< NSObject >
 
 @optional
 
+/*!
+ * @method      updaterShouldCheckForDrafts:
+ * @abstract    Implement if you need to check for draft updates.
+ * @param       updater The updater object
+ * @result      YES if the updater should check for draft updates, otherwise NO
+ */
 - ( BOOL )updaterShouldCheckForDrafts: ( GitHubUpdater * )updater;
+
+/*!
+ * @method      updaterShouldCheckForPrereleases:
+ * @abstract    Implement if you need to check for prerelease updates.
+ * @param       updater The updater object
+ * @result      YES if the updater should check for prerelease updates, otherwise NO
+ */
 - ( BOOL )updaterShouldCheckForPrereleases: ( GitHubUpdater * )updater;
+
+/*!
+ * @method      updaterShouldCheckForUpdatesInBackground:
+ * @abstract    Whether the updater is allowed to check for updates in background.
+ * @discussion  By implementing this method, you can control if the updater can
+ *              check for updates in background.
+ *              This method has no effect on user-initiated update checks, using
+ *              `- [ GitHubUpdater checkForUpdates: ]`. It will only be able to
+ *              control `- [ GitHubUpdater checkForUpdatesInBackground ]`.
+ * @param       updater The updater object
+ * @result      YES if the updater should check for updates in background, otherwise NO
+ */
 - ( BOOL )updaterShouldCheckForUpdatesInBackground: ( GitHubUpdater * )updater;
+
+/*!
+ * @method      classForUpdaterProgressWindowController:
+ * @abstract    Returns the class to use for the progress window controller.
+ * @discussion  This allows you to specify a custom class for the progress
+ *              window controller, if you need customization.
+ *              Note that the returned class must inherit from
+ *              `GitHubProgressWindowController`.
+ * @param       updater The updater object
+ * @result      The class to use
+ */
 - ( Class )classForUpdaterProgressWindowController: ( GitHubUpdater * )updater;
+
+/*!
+ * @method      classForUpdaterInstallWindowController:
+ * @abstract    Returns the class to use for the install window controller.
+ * @discussion  This allows you to specify a custom class for the install
+ *              window controller, if you need customization.
+ *              Note that the returned class must inherit from
+ *              `GitHubInstallWindowController`.
+ * @param       updater The updater object
+ * @result      The class to use
+ */
 - ( Class )classForUpdaterInstallWindowController: ( GitHubUpdater * )updater;
+
+/*!
+ * @method      updater:urlForUpdatesWithUser:repository:proposedURL:
+ * @abstract    Returns the URL for the update check.
+ * @discussion  You may implement this if you need to customize the update
+ *              URL, for instance if you use another servcie than GitHub.
+ * @param       updater     The updater object
+ * @param       user        The user name
+ * @param       repository  The repository name
+ * @param       proposedURL The proposed, default URL
+ */
 - ( NSURL * )updater: ( GitHubUpdater * )updater urlForUpdatesWithUser: ( NSString * )user repository: ( NSString * )repository proposedURL: ( NSURL * )proposedURL;
+
+/*!
+ * @method      updater:willShowProgressWindowController:
+ * @abstract    Called when the updater is about to show a progress window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater willShowProgressWindowController: ( GitHubProgressWindowController * )controller;
+
+/*!
+ * @method      updater:willShowInstallWindowController:
+ * @abstract    Called when the updater is about to show an install window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater willShowInstallWindowController: ( GitHubInstallWindowController * )controller;
+
+/*!
+ * @method      updater:didShowProgressWindowController:
+ * @abstract    Called when the updater has shown a progress window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater didShowProgressWindowController: ( GitHubProgressWindowController * )controller;
+
+/*!
+ * @method      updater:didShowProgressWindowController:
+ * @abstract    Called when the updater has shown an install window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater didShowInstallWindowController: ( GitHubInstallWindowController * )controller;
+
+/*!
+ * @method      updater:willCloseProgressWindowController:
+ * @abstract    Called when the updater is about to close a progress window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater willCloseProgressWindowController: ( GitHubProgressWindowController * )controller;
+
+/*!
+ * @method      updater:willCloseInstallWindowController:
+ * @abstract    Called when the updater is about to close an install window.
+ * @param       updater     The updater object
+ * @param       controller  The window controller
+ */
 - ( void )updater: ( GitHubUpdater * )updater willCloseInstallWindowController: ( GitHubInstallWindowController * )controller;
+
+/*!
+ * @method      updater:willDisplayAlert:withError:
+ * @abstract    Called when the updater is about to display an error alert.
+ * @param       updater The updater object
+ * @param       alert   The alert object
+ * @param       error   The error object
+ */
 - ( void )updater: ( GitHubUpdater * )updater willDisplayAlert: ( NSAlert * )alert withError: ( NSError * )error;
+
+/*!
+ * @method      updater:willDisplayUpToDateAlert:
+ * @abstract    Called when the updater is about to display an up-to-date alert.
+ * @param       updater The updater object
+ * @param       alert   The alert object
+ */
 - ( void )updater: ( GitHubUpdater * )updater willDisplayUpToDateAlert: ( NSAlert * )alert;
 
 @end
